b704a6221cac25c310b0e097564d2d91b81e6666
Anahi Quezada <anahi.quezada@udla.edu.ec>
2024-05-25 14:32:25 -0500

    Cambios Maui App

---
+++ ProyectoResenaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoResenaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProyectoResenaApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoResenaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
