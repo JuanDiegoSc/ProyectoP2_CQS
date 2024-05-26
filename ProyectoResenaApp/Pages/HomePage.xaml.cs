0f34939b836459f50296728ce6248033e7c1de9e
Sebastián Nicolas Casamen Cuichan <sebastian.casamen@udla.edu.ec>
2024-05-26 17:47:57 -0500

    Intento Carrusel

--- ProyectoResenaApp/Pages/HomePage.xaml.cs
+++ ProyectoResenaApp/Pages/HomePage.xaml.cs
@@ -1,9 +1,19 @@
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
 namespace ProyectoResenaApp.Pages;
-
 public partial class HomePage : ContentPage
 {
-	public HomePage()
+    public ObservableCollection<ImagenesCarrusel> Resources { get; set; }
+    public HomePage()
 	{
 		InitializeComponent();
-	}
+
+        Resources = new ObservableCollection<ImageResource>{
+        new ImageResource { ImagePath = "tou1.jpg", Title = "The Last Of Us Part I" },
+        new ImageResource { ImagePath = "sekiro.jpg", Title = "Sekiro Shadows Die Twice" },
+        new ImageResource { ImagePath = "liesp.jpg", Title = "Lies Of P" },
+    };
+
+        BindingContext = this;
+    }
 }
