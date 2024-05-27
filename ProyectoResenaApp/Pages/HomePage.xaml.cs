658a492e78663125feb0ce7a1d01e840daa2f8c7
Juan Diego Silva Castillo <juan.silva.castillo@udla.edu.ec>
2024-05-26 19:08:53 -0500

    Commit homePage actualizado

--- ProyectoResenaApp/Pages/HomePage.xaml.cs
+++ ProyectoResenaApp/Pages/HomePage.xaml.cs
@@ -1,8 +1,31 @@
 namespace ProyectoResenaApp.Pages;
+using ProyectoResenaApp.Models;
+using System.Collections.ObjectModel;
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<Carrusel>? Carrusel1 { get; set; }
+    public ObservableCollection<Carrusel>? Carrusel2 { get; set; }
+    public HomePage()
+    {
+        InitializeComponent();
+        InitializeGames();
+        BindingContext = this;
+    }
+
+    private void InitializeGames()
+    {
+        Carrusel1 = new ObservableCollection<Carrusel>
+        {
+             new Carrusel { Name = "The Last Of Us", Image = "tlou.png" },
+             new Carrusel { Name = "Fallout Shelter", Image = "imgfallout.png" },
+             new Carrusel { Name = "Sekiro: Shadows Die Twice", Image = "sekiro.png" }
+        };
+
+        Carrusel2 = new ObservableCollection<Carrusel>
+        {
+            new Carrusel { Name = "The Last Of Us", Image = "tlou.png" },
+            new Carrusel { Name = "Fallout Shelter",Image = "imgfallout.png" },
+            new Carrusel { Name = "Sekiro: Shadows Die Twice", Image = "sekiro.png" }
+  };
+    }
 }
